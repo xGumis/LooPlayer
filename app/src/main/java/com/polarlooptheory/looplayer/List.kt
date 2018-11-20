c3cab6de247dd47032e186e111a7555f6ce4bc56
xGumis <39995960+xGumis@users.noreply.github.com>
2018-11-20 09:35:52 +0100

    Init

---
+++ app/src/main/java/com/polarlooptheory/looplayer/List.kt
@@ -0,0 +1,12 @@
+package com.polarlooptheory.looplayer
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class List : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_list)
+    }
+}
