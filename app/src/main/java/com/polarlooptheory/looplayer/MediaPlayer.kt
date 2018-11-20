7c893d0373c5bcbe6ab860919359908e79984c14
xGumis <39995960+xGumis@users.noreply.github.com>
2018-11-20 09:52:56 +0100

    start

---
+++ app/src/main/java/com/polarlooptheory/looplayer/MediaPlayer.kt
@@ -0,0 +1,12 @@
+package com.polarlooptheory.looplayer
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MediaPlayer : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_media_player)
+    }
+}
